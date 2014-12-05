464c305540d87a846ed1ca19ed6f1abf349a004b
YuChul YANG <yangyuchul@gmail.com>
2014-12-05 16:28:11 +0900

    Add cmsCondorStatus.sh cmsCondorStatus

---
+++ cmsCondorStatus
@@ -0,0 +1,1 @@
+cmsCondorStatus.sh
---
+++ cmsCondorStatus.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+dir=$1
+totalJobs=""
+nJobs=0
+nTotalJobs=0
+files=`find $dir/log/ -maxdepth 1 -name "*.stdout"`
+nRoot=`ls $dir/output/*.root | wc -l`
+nGoodLog=`grep "### END Final cmsRun JobEnd 0 TryNumber" $dir/log/*stdout | wc -l`
+for file in $files
+do
+	((nTotalJobs++))
+	bName=`basename $file`
+	if [ "`ls -al $file  | awk '{print $5}'`" == "0" ]; then
+		name=${bName/outCondor_/}
+		name=${name/.stdout/}
+		totalJobs="${totalJobs} $name"
+		((nJobs++))
+		echo "######### CondorJob $name ##########################################################"
+		condor_tail -maxbytes 102400 $name
+		echo "####################################################################################"
+		sleep 5
+		echo ""
+		echo ""
+	fi
+done
+
+echo "FinalStatus $nJobs / $nTotalJobs [RooFile $nRoot GoodLog $nGoodLog] RunningJobId : $totalJobs $dir"
+
+
